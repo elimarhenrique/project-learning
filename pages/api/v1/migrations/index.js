5e2dc519248554fdb5c7534ab69b36eb8c4e568c
elimarhenrique <elimar.henrique@yahoo.com.br>
2026-01-21 00:59:27 -0300

    redeploy preview

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,22 +1,22 @@
-import migrationRunner from "node-pg-migrate"
-import { join } from "node:path"
-import database from "infra/database.js"
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
 
-export default async function migrations(request, response) { 
+export default async function migrations(request, response) {
   const dbClient = await database.getNewClient();
   const defaultMigrationOptions = {
-      dbClient: dbClient,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-  }
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
 
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationOptions);
     await dbClient.end();
-    return response.status(200).json(pendingMigrations);    
+    return response.status(200).json(pendingMigrations);
   }
 
   if (request.method === "POST") {
@@ -30,9 +30,8 @@ export default async function migrations(request, response) {
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     }
-    return response.status(200).json(migratedMigrations);    
+    return response.status(200).json(migratedMigrations);
   }
-  
-  return response.status(405).end()
 
+  return response.status(405).end();
 }
